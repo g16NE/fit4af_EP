50d48ef7f24384abfe5a146a732d91efc4bf486e
G16NE <hsvne11@hotmail.de>
2015-09-24 17:44:49 +0200

    LinuxDocs

--- Ruby/Code/studio_game3.rb
+++ Ruby/Code/studio_game3.rb
@@ -4,14 +4,39 @@ class Player
 		@health = phealth
 	end
 
+def say_hello
+	"Ich bin #{@name} mit einem Wert von #{@health}"
+end
+
+def to_s
+		say_hello
+end
 
 	def blam
 	@health = @health + 10
+	puts "Tom got blamed"
 	end
 
 	def w00t
 	@health = @health -10
+	puts "Tom got w00ted"
 	end
 
-	player1 = player.new("Mike", 50)
-	player2 = player.new("Karl")
+end
+	Player1 = Player.new("Mike", 50)
+	puts Player1
+
+	player2 = Player.new("Karl")
+	puts Player1.say_hello
+	
+	Player1.blam
+	puts Player1
+
+	Player1.w00t
+	puts Player1
+
+	
+
+
+
+
