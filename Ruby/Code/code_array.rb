845aefbe85322470940d19db0f077dcbac726b5d
G16NE <hsvne11@hotmail.de>
2015-11-19 19:19:35 +0100

    Linux

---
+++ Ruby/Code/code_array.rb
@@ -0,0 +1,20 @@
+3.times do |number|
+	# puts number.to_s
+end
+lotto = []
+7.times do
+	lotto << rand(1..49)
+end
+
+numbers = (1..10).to_a
+var = numbers.select do|n|
+	n.even?
+end
+puts var
+
+evens, odds = numbers.partition do |n|
+	n.even?
+end
+puts evens
+puts "-------------"
+puts odds
--- Ruby/Code/studio_game5.1.rb
+++ Ruby/Code/studio_game5.1.rb
@@ -1,18 +1,42 @@
-class Game
+# a. erstellen Sie eine Klasse Game mit einem Konstruktor,
+# 		der eine Instanzvariable title anlegt und fuellt und ein leeres Array mit dem Name Player enthaelt.
+# b. die Klasse Game hat eine Methode add_player der ein SPieler hinzugefuegt wird
+# c. eine Methode play erzeugt folgenden Ausdruck:
+# 	There are 3 players in Knuckleheads: 
+# 	I'm Moe with a health of 100 and a score of 103.
+# 	I'm Larry with a health of 60 and a score of 65.
+# 	I'm Curly with a health of 125 and a score of 130.
+# 	Moe got blammed!
+# 	Moe got w00ted!
+# 	Moe got w00ted!
+# 	I'm Moe with a health of 120 and a score of 123.
+# 	Larry got blammed!
+# 	Larry got w00ted!
+# 	Larry got w00ted!
+# 	I'm Larry with a health of 80 and a score of 85.
+# 	Curly got blammed!
+# 	Curly got w00ted!
+# 	Curly got w00ted!
+# 	I'm Curly with a health of 145 and a score of 150.
 
+class Game
 	def initialize(title)
 		@title = title
 		@player = []
 	end
-	def add_movie(player)
-		@player << Spieler
+	def add_player(name)
+		@player << name
 	end
 	def play
-		puts "There are 3 players in Knuckleheads:"
-		@player.each do |player|
-			player.thumbs_up
-			player.thumbs_up
-			puts player.to_s
+		puts "There are #{@player.count} players in #{@title}:"
+		@player.each do |status|
+			puts status
+		end
+		@player.each do |change|
+			change.blam
+			change.woot
+			change.woot
+			puts change
 		end
 	end
 end
@@ -21,53 +45,38 @@ class Player
 
 	attr_reader :health
 	attr_accessor :name
-
-	def initialize(pname, phealth=100)
-		@name = pname.capitalize
-		@health = phealth
+	def initialize(name, health=100)
+		@name = name.capitalize
+		@health = health
 	end
-
-	def say_hello
-		"Ich bin #{@name} mit einem Wert von #{score}"
-	end
-
-	def to_s
-		say_hello
-	end
-
-	def blam
-		@health = @health + 10
-		puts "#{@name} got blamed"
-	end
-	
-	def w00t
-		@health = @health - 10
-		puts "#{@name} got w00ted"
-	end
-
 	def score
 		@health + @name.length
 	end
+	def say_hello
+		"I'm #{@name} with a health of #{@health} and a score of #{score}."
+	end
+	def blam
+		@health += 10
+		puts "#{@name} got blammed!"
+	end
+	def woot
+		@health -= 10
+		puts "#{@name} got w00ted!"
+	end
+	def to_s
+		say_hello
+	end
 end
 
+player1 = Player.new("Moe")
+player2 = Player.new("Larry", 60)
+player3 = Player.new("Curly", 125)
+game = Game.new("Knuckleheads")
+game.add_player(player1)
+game.add_player(player2)
+game.add_player(player3)
 
-player1 = Player.new("Mike", 50)
-player2 = Player.new("Karl")
-game = Game.new('Horrorfilme')
-game.add_Spieler(player1)
-game.add_Spieler(player2)
 game.play
-	# Player1 = Player.new("Mike", 50)
-	
-	# Player2 = Player.new("Karl")
 
-	# Player3 = Player.new("Tom")
-
-	# players = [Player1, Player2, Player3]
-
-	# puts "Anzahl der Spieler: #{players.count}"
-
-	# players.each do |player|
-	# puts player
-# end
+puts "#{@player}"
 	
